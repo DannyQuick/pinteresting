ed5ff9f7d41026a33057c9320956802a229c8136
Danny Quick <yo@dannyquikc.nyc>
2016-03-21 15:49:10 -0400

    fixing database

--- db/migrate/20160319193913_add_user_id_to_pins.rb
+++ db/migrate/20160319193913_add_user_id_to_pins.rb
@@ -1,7 +1,6 @@
 class AddUserIdToPins < ActiveRecord::Migration
   def change
-    add_column :pins, :user_id, :string
-    add_column :pins, :, :integer
-    add_index :pins, :
+    add_column :pins, :user_id, :integer
+    add_index :pins, :user_id
   end
 end
